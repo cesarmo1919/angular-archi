663de1e0351b32a0df25f0cfc29400a41cc44c8e
Nader Laroussi <nader.laroussi@gmail.com>
2021-01-28 08:06:24 +0100

    Initial commit

---
+++ projects/angular-app/src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule
+  ]
+})
+export class CoreModule { }
